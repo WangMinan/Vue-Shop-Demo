3190e4acecf5ceac4db89fb6210863fd09c93344
WangMinan <wangminan0811@hotmail.com>
2022-09-18 21:53:19 +0800

    完成登录界面的UI制作
    尝试使用了阿里图标库但是失败了

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <h1>登陆组件</h1>
+  <div class = "login-container">
+    <div class="login-box">
+      <div class = "avatar-box">
+        <img src = "@/assets/logo.png" >
+      </div>
+<!--      label-width用于左右对齐-->
+      <el-form label-width="0px" class = "login-form">
+<!--        用户名-->
+        <el-form-item>
+          <el-input prefix-icon= "el-icon-user"></el-input>
+        </el-form-item>
+<!--        密码-->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-lock"></el-input>
+        </el-form-item>
+        <el-form-item class = "buttons">
+<!--          登录-->
+          <el-button type="primary">登录</el-button>
+<!--        重置-->
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -12,5 +33,47 @@ export default options
 </script>
 
 <style lang="less" scoped>
-
+.login-container{
+  background-color: #2af598;
+  height: 100%;
+}
+.login-box{
+  width: 450px;
+  height: 300px;
+  background-color: white;
+  border-radius: 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  .avatar-box{
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eeee;
+    border-radius: 50%;
+    padding: 10px; /* 图片边框间隙 */
+    background-color: white;
+    box-shadow: 0 0 10px #dddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%,-50%); /* 相对自身 */
+    img{
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eeee;
+    }
+  }
+}
+.buttons{
+  display: flex;
+  justify-content: flex-end; /* 右对齐 */
+}
+.login-form{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import './plugins/element.js'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 全局css
+import '@/assets/css/global.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)

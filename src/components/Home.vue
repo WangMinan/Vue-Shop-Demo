063c60cce9244d30889be1de3c600696cf2ff3b3
WangMinan <wangminan0811@hotmail.com>
2022-09-19 21:58:59 +0800

    退出功能制作完成 登录页完全完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
+    <el-button type="info" @click = 'logout'>退出</el-button>
   </div>
 </template>
 
 <script>
 const options = {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 export default options
 </script>
 
-<style scoped>
+<style lang = 'less' scoped>
 
 </style>
